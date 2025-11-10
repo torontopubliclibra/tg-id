58c416e00e79c01ab7b481d7eb6581f767755d11
torontopubliclibra <dana.r.teagle@gmail.com>
2025-11-09 22:19:15 -0500

    feat: various improvements

--- src/app/components/topBar.tsx
+++ src/app/components/topBar.tsx
@@ -20,12 +20,13 @@ export default function TopBar() {
         <div className="top-bar">
             <Link href="/" onClick={closeMobileNav}>
                 <h1>
-                    <Image src="/logo.png" alt="I.D. Guide logo" width={115} height={50}/>
+                    <Image src="/logo.png" alt="I.D. Guide logo" width={97} height={45}/>
+                    {/* I.D. guide */}
                 </h1>
             </Link>
             <Nav mobileOpen={isMobileNavOpen} closeMobileNav={closeMobileNav}/>
             <button onClick={toggleMobileNav}>
-                <Image src="/menu.svg" alt="Menu icon" width={32} height={32}/>
+                <Image src="/menu-3.svg" alt="Menu icon" width={30} height={30}/>
             </button>
         </div>
     );
